afba5d0111a4869104d36c6efd944281827d516d
busterwood <chris.j.austin@gmail.com>
2017-06-24 13:22:24 +0100

    Add factory expression to test program.  Move some schema code gen to separate file

---
+++ BusterWood.Data/HasSchemaBuilder.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using static System.Reflection.MethodAttributes;
+using static System.Reflection.CallingConventions;
+using System.Collections;
+
+namespace BusterWood.Data
+{
+    public static class HasSchemaBuilder
+    {
+        public static MethodBuilder DefineSchemaHash(TypeBuilder builder, int schemaHashCode)
+        {
+            var method = builder.DefineMethod("SchemaHashCode", Public | Virtual | Final, HasThis, typeof (int), Type.EmptyTypes);
+            var il = method.GetILGenerator();
+            il.Constant(schemaHashCode);
+            il.Return();
+            return method;
+        }
+
+        public static MethodBuilder DefineGetEnumerator(TypeBuilder builder, ConstructorBuilder enumeratorCtor)
+        {
+            var method = builder.DefineMethod("IEnumerable.GetEnumerator", Private | Virtual | Final | HideBySig, HasThis, typeof (IEnumerator), Type.EmptyTypes);
+            var il = method.GetILGenerator();
+            il.This().New(enumeratorCtor);
+            il.Return();
+            builder.DefineMethodOverride(method, typeof (IEnumerable).GetMethod(nameof(IEnumerable.GetEnumerator)));
+            return method;
+        }
+
+        public static void DefineGetGenericEnumerator(TypeBuilder builder, ConstructorBuilder enumeratorCtor)
+        {
+            var method = builder.DefineMethod(nameof(IEnumerable.GetEnumerator), Public | Virtual | Final | HideBySig, HasThis, typeof (IEnumerator<ColumnValue>), Type.EmptyTypes);
+            var il = method.GetILGenerator();
+            il.This().New(enumeratorCtor);
+            il.Return();
+        }
+
+        public static MethodBuilder DefineEqualsObject(TypeBuilder builder, MethodBuilder equalsHasSchema)
+        {
+            var method = builder.DefineMethod("Equals", Public | Virtual | HideBySig, HasThis, typeof (bool), new[]{typeof (object)});
+            var il = method.GetILGenerator();
+            il.This();
+            il.Arg1().AsType(typeof (IHasSchema));
+            il.CallVirt(equalsHasSchema);
+            il.Return();
+            builder.DefineMethodOverride(method, typeof (object).GetMethod(nameof(object.Equals), new[]{typeof (object)}));
+            return method;
+        }
+
+        public static MethodBuilder DefineEqualsHasSchema(TypeBuilder builder, List<PropertyBuilder> props, MethodBuilder schemaHasCode)
+        {
+            var method = builder.DefineMethod("Equals", Public | Virtual | Final | HideBySig, HasThis, typeof (bool), new[]{typeof (IHasSchema)});
+            var il = method.GetILGenerator();
+            var em = il.DeclareLocal<IEnumerator<ColumnValue>>();
+            var cv = il.DeclareLocal<ColumnValue>();
+            var notEqual = il.DefineLabel();
+            CheckOtherNotNull(il, notEqual);
+            CheckSchemaHash(schemaHasCode, il, notEqual);
+            GetOtherEnumerator(il, em);
+            foreach (var p in props.OrderBy(p => p.Name, StringComparer.OrdinalIgnoreCase))
+            {
+                MoveNext(il, em, notEqual);
+                IsPropertyEqual(il, em, notEqual, p, cv);
+            }
+
+            il.Return(true);
+            il.MarkLabel(notEqual);
+            il.Return(false);
+            return method;
+        }
+
+        private static void CheckOtherNotNull(ILGenerator il, Label notEqual)
+        {
+            // if (other == null) return false;
+            il.Arg1();
+            il.BranchIfFalse(notEqual);
+        }
+
+        private static void CheckSchemaHash(MethodBuilder schemaHasCode, ILGenerator il, Label notEqual)
+        {
+            // if (other.SchemaHashCode() != SchemaHashCode()) return false;
+            il.This().Call(schemaHasCode);
+            il.Arg1().CallVirt<IHasSchema>(nameof(IHasSchema.SchemaHashCode));
+            il.BranchIfNotEqual(notEqual);
+        }
+
+        private static void GetOtherEnumerator(ILGenerator il, LocalBuilder em)
+        {
+            // em = other.GetEnumerator();
+            il.Arg1().CallVirt<IEnumerable<ColumnValue>>("GetEnumerator");
+            il.Store(em);
+        }
+
+        private static void MoveNext(ILGenerator il, LocalBuilder em, Label notEqual)
+        {
+            // if (!e.MoveNext()) return false;
+            il.Load(em).CallVirt<IEnumerator>("MoveNext");
+            il.BranchIfFalse(notEqual);
+        }
+
+        private static void IsPropertyEqual(ILGenerator il, LocalBuilder em, Label notEqual, PropertyBuilder p, LocalBuilder cv)
+        {
+            //if (!Equals(Id, e.Current.Value)) return false;
+            il.This().Call(p.GetMethod);
+            if (p.PropertyType.IsValueType)
+                il.Box(p.PropertyType);
+            il.Load(em).GetPropertyValue<IEnumerator<ColumnValue>>("Current");
+            il.Store(cv); // store column value
+            il.LoadAddress(cv); // push reference to column value
+            il.Call(typeof (ColumnValue).GetProperty("Value").GetMethod);
+            il.Call(typeof (object).GetMethod("Equals", BindingFlags.Static | BindingFlags.Public));
+            il.BranchIfFalse(notEqual);
+        }
+
+        internal static void DefineGetHashCode(TypeBuilder type, MethodBuilder schemaHashCode, IEnumerable<PropertyInfo> props)
+        {
+            var method = type.DefineMethod("GetHashCode", Public | Virtual | Final | HideBySig, HasThis, typeof (int), null);
+            var il = method.GetILGenerator();
+            var locals = props.Select(p => p.PropertyType.IsValueType ? il.DeclareLocal(p.PropertyType) : null).ToList();
+            il.This().Call(schemaHashCode);
+            int i = 0;
+            foreach (var p in props)
+            {
+                il.This().CallGetProperty(p);
+                if (locals[i] != null)
+                {
+                    il.Store(locals[i]);
+                    il.LoadAddress(locals[i]);
+                    il.Call(p.PropertyType.GetMethod("GetHashCode"));
+                }
+                else
+                    il.CallVirt(p.PropertyType.GetMethod("GetHashCode"));
+                il.Emit(OpCodes.Add);
+                i++;
+            }
+
+            il.Return();
+            type.DefineMethodOverride(method, typeof (object).GetMethod("GetHashCode"));
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var c1 = new Class1("fred", 1, DateTime.Now);
+            var c1 = new Class1("fred", 1, DateTime.UtcNow);
             var t1 = TypeExtender.Extend(typeof(Class1), new Column("Size", typeof(double)), new Column("Status", typeof(string)));
             var ext1 = (IHasSchema)Activator.CreateInstance(t1, c1, 2d, "bad");
             foreach (var cv in ext1)
@@ -26,13 +27,20 @@ namespace ConsoleApplication1
             //var t2 = TypeRestrictor.Restrict(ext1.GetType(), new string[] { "Id", "Size" });
             //var ext2 = Activator.CreateInstance(t2, ext1);
 
-            var sw = new Stopwatch();
-            var seq = Enumerable.Range(1, 100000)
+            //TODO: factory method - if fac just in Extend(....)
+            var p1 = Expression.Parameter(typeof(Class1));
+            var p2 = Expression.Parameter(typeof(double));
+            var p3 = Expression.Parameter(typeof(string));
+            var lambda = Expression.Lambda<Func<Class1, double, string, object>>(Expression.New(t1.GetConstructor(new[] { typeof(Class1), typeof(double), typeof(string) }), p1, p2, p3), p1, p2, p3);
+            var factory = lambda.Compile();
+
+            var seq = Enumerable.Range(1, 10000000)
                 .Select(i => new Class1("hello", i, new DateTime(i)))
-                .Select(c => Activator.CreateInstance(t1, c, c.Id * 2.5d, "bad")) //TODO: factory method - if fac just in Extend(....)
-                .ToList();
+                .Select(c => factory(c, c.Id * 2.5d, "bad")) 
+                ;
+            var sw = new Stopwatch();
             sw.Start();
-            foreach (dynamic item in seq.Distinct())
+            foreach (dynamic item in seq)
             {
                 GC.KeepAlive(item.Text);
                 GC.KeepAlive(item.Id);
@@ -41,6 +49,7 @@ namespace ConsoleApplication1
             }
             sw.Stop();
             Console.WriteLine($"took {sw.ElapsedMilliseconds} MS");
+            Console.ReadLine();
         }
     }
 }
