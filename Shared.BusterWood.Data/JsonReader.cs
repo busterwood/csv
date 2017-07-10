8c6a8e0821d4322d0d93cfa5b358c484232e292e
BusterWood <busterwood@users.noreply.github.com>
2017-07-10 17:35:25 +0100

    Add simple JSON parser

---
+++ Shared.BusterWood.Data/JsonReader.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BusterWood.Json
+{
+    /// <summary>Allows enumeration of a string of JSON tokens</summary>
+    /// <remarks>NOT safe for multiple or concurrent iteration</remarks>
+    public class JsonReader : IEnumerable<JsonToken>
+    {
+        readonly StringBuilder sb = new StringBuilder();
+        readonly TextReader input;
+
+        public JsonReader(TextReader input)
+        {
+            this.input = input ?? throw new ArgumentNullException(nameof(input));
+        }
+
+        public IEnumerator<JsonToken> GetEnumerator()
+        {
+            for(;;)
+            {
+                int n = input.Read();
+                if (n == -1)
+                    yield break;
+                var curr = (char)n;
+                if (char.IsWhiteSpace(curr))
+                    continue;
+                switch ((JsonType)curr)
+                {
+                    case JsonType.StartArray:
+                        yield return new JsonToken(JsonType.StartArray, "["); // use literal singleton string, as Char.ToString() causes allocation
+                        break;
+                    case JsonType.EndArray:
+                        yield return new JsonToken(JsonType.EndArray, "]");
+                        break;
+                    case JsonType.StartObject:
+                        yield return new JsonToken(JsonType.StartObject, "{");
+                        break;
+                    case JsonType.EndObject:
+                        yield return new JsonToken(JsonType.EndObject, "}");
+                        break;
+                    case JsonType.Comma:
+                        yield return new JsonToken(JsonType.Comma, ",");
+                        break;
+                    case JsonType.Colon:
+                        yield return new JsonToken(JsonType.Colon, ":");
+                        break;
+                    case JsonType.String:
+                        yield return ReadString();
+                        break;
+                    case JsonType.True:
+                        yield return ReadTrue();
+                        break;
+                    case JsonType.False:
+                        yield return ReadFalse();
+                        break;
+                    case JsonType.Null:
+                        yield return ReadNull();
+                        break;
+                    default:
+                        if (char.IsNumber(curr) || curr == '-')
+                            yield return ReadNumber(curr);
+                        else
+                            throw new Exception($"Unexpected character '{curr}'");
+                        break;
+                }
+            }
+        }
+
+        private JsonToken ReadTrue()
+        {
+            Expect("true");
+            return new JsonToken(JsonType.True, "true");
+        }
+
+        private JsonToken ReadFalse()
+        {
+            Expect("false");
+            return new JsonToken(JsonType.False, "false");
+        }
+
+        private JsonToken ReadNull()
+        {
+            Expect("null");
+            return new JsonToken(JsonType.Null, "null");
+        }
+
+        private void Expect(string expected)
+        {
+            foreach (var c in expected.Skip(1)) /// skip current char, allows correct reporting of expected in exception
+            {
+                int n = input.Read();
+                if (n == -1)
+                    throw new Exception("Expected closing quote of string but did not find one");
+
+                var next = (char)n;
+                if (next != c)
+                    throw new Exception($"Expected '{c}' but got '{next}' when expected to read '{expected}'");
+            }
+        }
+
+        private JsonToken ReadString()
+        {
+            sb.Clear(); // reuse existing buffer
+            for (;;)
+            {
+                int n = input.Read();
+                if (n == -1)
+                    throw new Exception("Expected closing quote of string but did not find one");
+
+                var next = (char)n;
+                if (next == '"')
+                {
+                    if (input.Peek() != '"')
+                        return new JsonToken(JsonType.String, sb.ToString());
+                    input.Read(); // turn "" into one " in sb buffer
+                }
+                else if (next == '\\')
+                {
+                    switch (input.Peek())
+                    {
+                        case '/': next = '/'; break;
+                        case '\\': next = '\\'; break;
+                        case '"': next = '"'; break;
+                        case 'b': next = '\b'; break;
+                        case 'f': next = '\f'; break;
+                        case 'n': next = '\n'; break;
+                        case 'r': next = '\r'; break;
+                        case 't': next = '\t'; break;
+                        default:
+                            throw new Exception("Expected escape sequence in string");
+
+                    }
+                    input.Read(); // consume after peeking
+                }
+                else if (next == '\r' || next == '\n')
+                    throw new Exception("Expected closing quote of string found end of line");
+
+                sb.Append(next);
+            }
+        }
+
+        private JsonToken ReadNumber(char first)
+        {
+            sb.Clear(); // reuse buffer
+            sb.Append(first);
+            for(;;)
+            {
+                int n = input.Peek();
+                if (n == -1)
+                    return new JsonToken(JsonType.Number, sb.ToString());
+                var next = (char)n;
+                if (next == '.')
+                    break;
+                if (!char.IsNumber(next))
+                    return new JsonToken(JsonType.Number, sb.ToString());
+                input.Read(); // consume from stream
+                sb.Append(next);
+            }
+
+            // peeked a '.' if we got here
+            if (sb[sb.Length - 1] == '-')
+                throw new Exception("Expected whole number part before the decimal point");
+            input.Read(); // consume from stream
+            sb.Append('.');
+
+            for (;;)
+            {
+                int n = input.Peek();
+                if (n == -1)
+                    break;
+                var next = (char)n;
+                if (next == '.')
+                    throw new Exception("Expected fractional number part but got another decimal point");
+                if (!char.IsNumber(next))
+                    break;
+                input.Read(); // consume from stream
+                sb.Append(next);
+            }
+
+            if (sb[sb.Length - 1] == '.')
+                throw new Exception("Expected fractional number part after then decimal point");
+            return new JsonToken(JsonType.Number, sb.ToString());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public struct JsonToken
+    {
+        public JsonType Type { get; }
+        public string Value { get; }
+
+        public JsonToken(JsonType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+        public override string ToString() => $"{Value} ({Type})";
+    }
+
+    public enum JsonType
+    {
+        StartArray = '[',
+        EndArray = ']',
+        StartObject = '{',
+        EndObject = '}',
+        Comma = ',',
+        Colon = ':',
+        String = '"',
+        True = 't',
+        False = 'f',
+        Null = 'n',
+        Number,
+    }
+}
---
+++ UnitTests/JsonReaderTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using BusterWood.Json;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class JsonReaderTests
+    {
+        [Test]
+        public void can_parse_empty_string()
+        {
+            var r = new JsonReader(new StringReader(""));
+            Assert.IsFalse(r.Any());
+        }
+
+        [TestCase("[", JsonType.StartArray)]
+        [TestCase("]", JsonType.EndArray)]
+        [TestCase("{", JsonType.StartObject)]
+        [TestCase("}", JsonType.EndObject)]
+        [TestCase(":", JsonType.Colon)]
+        [TestCase(",", JsonType.Comma)]
+        [TestCase("true", JsonType.True)]
+        [TestCase("false", JsonType.False)]
+        [TestCase("null", JsonType.Null)]
+        [TestCase("1", JsonType.Number)]
+        [TestCase("1234", JsonType.Number)]
+        [TestCase("1.1", JsonType.Number)]
+        [TestCase("-1.1", JsonType.Number)]
+        [TestCase("0.1234", JsonType.Number)]
+        public void can_parse_single_token(string input, JsonType expected)
+        {
+            var r = new JsonReader(new StringReader(input));
+            var toks = r.ToList();
+            Assert.AreEqual(1, toks.Count, "count");
+            var t = toks[0];
+            Assert.AreEqual(expected, t.Type);
+            Assert.AreEqual(input, t.Value);
+        }
+
+        [TestCase("", JsonType.String, "")]
+        [TestCase("a", JsonType.String, "a")]
+        [TestCase("abc", JsonType.String, "abc")]
+        [TestCase(@"a\tbc", JsonType.String, "a\tbc")]
+        [TestCase(@"a\nbc", JsonType.String, "a\nbc")]
+        [TestCase(@"a\\bc", JsonType.String, @"a\bc")]
+        //[TestCase("a\fbc", JsonType.String)]
+        //[TestCase("a\nbc", JsonType.String)]
+        //[TestCase("a\rbc", JsonType.String)]
+        //[TestCase("a\"bc", JsonType.String)]
+        public void can_parse_single_string_token(string input, JsonType expectedType, string expectedText)
+        {
+            var r = new JsonReader(new StringReader("\"" + input + "\""));
+            var toks = r.ToList();
+            Assert.AreEqual(1, toks.Count, "count");
+            var t = toks[0];
+            Assert.AreEqual(expectedType, t.Type);
+            Assert.AreEqual(expectedText, t.Value);
+        }
+
+        [Test]
+        public void can_read_multiple_tokens()
+        {
+            var r = new JsonReader(new StringReader(@"[
+  {  ""one"": 1, ""hello"": ""world"" }
+]"));
+            var toks = r.ToList();
+            int i = 0;
+            Assert.AreEqual(new JsonToken(JsonType.StartArray, "["), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.StartObject, "{"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.String, "one"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.Colon, ":"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.Number, "1"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.Comma, ","), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.String, "hello"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.Colon, ":"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.String, "world"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.EndObject, "}"), toks[i++]);
+            Assert.AreEqual(new JsonToken(JsonType.EndArray, "]"), toks[i++]);
+            Assert.AreEqual(i, toks.Count);
+        }
+    }
+}
