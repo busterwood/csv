99701150ac119812ac65903fd6dd5b06c8f15c80
Chris Austin <Christopher.Austin@gfk.com>
2021-05-21 17:05:48 +0100

    Fix hash code of columns to be case-insensitive

--- BusterWood.Data/Column.cs
+++ BusterWood.Data/Column.cs
@@ -34,9 +34,9 @@ namespace BusterWood.Data
         public object Type { get; }
 
         public bool NameEquals(string name) => NameEquality.Equals(Name, name);
-        public bool Equals(Column other) => NameEquals(other.Name) && Type == other.Type;
+        public bool Equals(Column other) => NameEquals(other.Name) && Equals(Type, other.Type);
         public override bool Equals(object obj) => obj is Column && Equals((Column)obj);
-        public override int GetHashCode() => (Name?.GetHashCode() ?? 0) ^ (Type?.GetHashCode() ?? 0);
+        public override int GetHashCode() => (Name != null ? NameEquality.GetHashCode(Name) : 0) ^ (Type?.GetHashCode() ?? 0);
         public override string ToString() => Name;
         public static bool operator ==(Column left, Column right) => left.Equals(right);
         public static bool operator !=(Column left, Column right) => !left.Equals(right);
